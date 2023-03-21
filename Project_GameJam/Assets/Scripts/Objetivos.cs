25afc29c493ea358117aeef58203a124defcccc3
Yachiryu <daniel.castrilln@misena.edu.co>
2023-03-21 13:15:44 -0500

    Creacion del primer script de los objetivos (Testeo)

--- Project_GameJam/Assets/Scripts/Objetivos.cs
+++ Project_GameJam/Assets/Scripts/Objetivos.cs
@@ -8,10 +8,33 @@ public class Objetivos : MonoBehaviour
 {
     public int numObjetivos;
     public TextMeshProUGUI textoMision;
-    public GameObject botonMision;
+    //public GameObject botonMision;
 
     private void Start()
     {
+        numObjetivos = GameObject.FindGameObjectsWithTag("Objetivo").Length;
+        textoMision.text = "Obten las esferas rojas" +
+                            "\n Restantes: " + numObjetivos;
+    }
+
+    private void Update()
+    {
         
+    }
+    private void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "Objetivo")
+        {
+            Destroy(col.transform.parent.gameObject);
+            numObjetivos--;
+            textoMision.text = "obten las esferas rojas" +
+                                "\n Restantes: " + numObjetivos;
+
+            if (numObjetivos <=0)
+            {
+                textoMision.text = "Recogiste las bolas :)";
+                
+            }
+        }
     }
 }
